73a6dedbce9628d926f3d84461b3685513c9535d
torviscoadri20 <torviscoadri20@gmail.com>
2024-03-19 14:44:52 -0500

    changues

---
+++ pages/page3.js
@@ -0,0 +1,166 @@
+document.getElementById('boton1').addEventListener('click', function() {
+    var audio = document.getElementById('audio1');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if  (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+
+
+  document.getElementById('boton2').addEventListener('click', function() {
+    var audio = document.getElementById('audio2');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+
+  document.getElementById('boton3').addEventListener('click', function() {
+    var audio = document.getElementById('audio3');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+
+
+  document.getElementById('boton4').addEventListener('click', function() {
+    var audio = document.getElementById('audio4');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+
+
+  document.getElementById('boton5').addEventListener('click', function() {
+    var audio = document.getElementById('audio5');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+
+  document.getElementById('boton6').addEventListener('click', function() {
+    var audio = document.getElementById('audio6');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+  document.getElementById('boton7').addEventListener('click', function() {
+    var audio = document.getElementById('audio7');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+  document.getElementById('boton8').addEventListener('click', function() {
+    var audio = document.getElementById('audio8');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+  document.getElementById('boton9').addEventListener('click', function() {
+    var audio = document.getElementById('audio9');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+  document.getElementById('boton10').addEventListener('click', function() {
+    var audio = document.getElementById('audio10');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+  document.getElementById('boton11').addEventListener('click', function() {
+    var audio = document.getElementById('audio11');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+  document.getElementById('boton12').addEventListener('click', function() {
+    var audio = document.getElementById('audio12');
+    if (audio.paused) {
+      audio.play();
+      this.textContent = 'Pausar Audio';
+    } else if (audio.currentTime > 0 && !audio.paused) {
+      audio.pause();
+      audio.currentTime = 0;
+      this.textContent = 'Reproducir Audio';
+    } else {
+      this.textContent = 'Te amo'
+    }
+  });
+
+  
